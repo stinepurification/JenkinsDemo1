dbe5685089a69dbd62962d7a5de8dc6d7696c2c3
stinepurification <stinepurification@gmail.com>
2019-07-06 07:35:12 -0500

    First Commit

---
+++ src/test/java/test/LoginTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+import page.LoginPage;
+import util.BrowserFactory;
+
+public class LoginTest {
+	
+@Test
+public void TechfiosLoginTest(){
+	
+WebDriver driver = BrowserFactory.startBrowser("chrome", "http://techfios.com/test/billing/?ng=login/");	
+	
+LoginPage logintotechfios= PageFactory.initElements(driver, LoginPage.class);
+
+logintotechfios.Login("techfiosdemo@gmail.com", "abc123");
+
+driver.close();
+
+driver.quit();
+
+}
+
+@Test
+public void TechfiosLoginTest2(){
+	
+WebDriver driver = BrowserFactory.startBrowser("chrome", "http://techfios.com/test/billing/?ng=login/");	
+	
+LoginPage logintotechfios= PageFactory.initElements(driver, LoginPage.class);
+
+logintotechfios.Login("techfiosdemo@gmail.com", "abc23");
+
+driver.close();
+
+driver.quit();
+
+}
+ 
+
+}
